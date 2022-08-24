60e4a7054469c3dc704bbe2804b88dcac9e62b6d
HP <samu.kauppi@metropolia.fi>
2022-08-24 10:27:46 +0300

    22/08/22 Muutos tehtävään 6

--- 2_Muuttjuat_Ja_Vuorovaikutteiset_Ohjelmat/Tehtävä_6.py
+++ 2_Muuttjuat_Ja_Vuorovaikutteiset_Ohjelmat/Tehtävä_6.py
@@ -13,3 +13,14 @@ for n in neliNumeroinenKoodi:
 
 print("Kolminumeroinen koodi: " + kolmiKoodi)
 print("Nelinumeroinen koodi: " + neliKoodi)
+
+# Keksin myöhemmin paremman tavan tehdä tämän
+# kolmiKoodi = ""
+# neliKoodi = ""
+#
+# for x in range(3):
+#     kolmiKoodi += str(random.randint(0, 9))
+# for x in range(4):
+#     neliKoodi += str(random.randint(1, 6))
+# print("Kolminumeroinen koodi: " + kolmiKoodi)
+# print("Nelinumeroinen koodi: " + neliKoodi)
