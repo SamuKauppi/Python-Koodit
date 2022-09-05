08461f45a0011a1f2f8e48a0b42e1ef254518df6
HP <samu.kauppi@metropolia.fi>
2022-09-05 09:30:33 +0300

    Moduuli 3 tehtävä 2 pieni optimointi (hyttiLuokka.lower)

--- 3_Valintarakenne(if)/Tehtävä_2.py
+++ 3_Valintarakenne(if)/Tehtävä_2.py
@@ -1,12 +1,12 @@
 hyttiLuokka = input("Syötä hyttiluokkasi: ")
-
-if(hyttiLuokka == "LUX"):
+hyttiLuokka = hyttiLuokka.lower();
+if hyttiLuokka == "lux":
     print("LUX on parvekkeellinen hytti yläkannella.")
-elif(hyttiLuokka == "A"):
+elif hyttiLuokka == "a":
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-elif(hyttiLuokka == "B"):
+elif hyttiLuokka == "b":
     print("B on ikkunaton hytti autokannen yläpuolella.")
-elif(hyttiLuokka == "C"):
+elif hyttiLuokka == "c":
     print("C on ikkunaton hytti autokannen alapuolella.")
 else:
     print("Virheellinen hyttiluokka")
