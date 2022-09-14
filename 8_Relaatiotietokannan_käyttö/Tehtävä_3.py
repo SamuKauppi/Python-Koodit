e61c374a0730200a0498903903d8164397bd72e4
HP <samu.kauppi@metropolia.fi>
2022-09-14 14:49:06 +0300

    Moduuli 8 Tehtävävien korjauksia

--- 8_Relaatiotietokannan_käyttö/Tehtävä_3.py
+++ 8_Relaatiotietokannan_käyttö/Tehtävä_3.py
@@ -14,7 +14,7 @@ def avaa_tietokanta():
 
 
 def etsi_lentoasemat(yhteys, maakoodi_a, maakoodi_b):
-    sql = "SELECT airport.latitude_deg, airport.longirude_deg FROM airport "
+    sql = "SELECT airport.latitude_deg, airport.longitude_deg FROM airport "
     sql += "WHERE airport.ident = '" + maakoodi_a + "' or airport.ident = '" + maakoodi_b + "'"
     kursori = yhteys.cursor()
     kursori.execute(sql)
@@ -22,8 +22,10 @@ def etsi_lentoasemat(yhteys, maakoodi_a, maakoodi_b):
     return tulos
 
 
+yhteys = avaa_tietokanta()
 maakoodi_a = input("Syötä ensimmäinen koodi: ")
 maakoodi_b = input("Syötä toinen koodi: ")
-yhteys = avaa_tietokanta()
+
 kaksi_asemaa = etsi_lentoasemat(yhteys, maakoodi_a, maakoodi_b)
-print(f"Lentoasemien välinen etäisyys on: {(distance.distance(kaksi_asemaa[0], kaksi_asemaa[1]))}")
+if len(kaksi_asemaa) > 0:
+    print(f"Lentoasemien välinen etäisyys on: {distance.distance(kaksi_asemaa[0], kaksi_asemaa[1])}")
